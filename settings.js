658f5bd900d4c1e974ab3e8fa89099d004dab0f1
MikelDelTio <mikeldeltio@outlook.com>
2014-01-30 18:34:32 +0100

    Initial commit

---
+++ settings.js
@@ -0,0 +1,18 @@
+var SETTINGS = (function() {
+	var private = {
+		'GCM_ID': 'AIzaSyCiWYVCwBJVfjg3Y9-CQm4VMe4eO1zsGgM',	//MUST be changed with you GCN_ID
+		'GCM_MSGCNT': '1',
+		'APN_GATEWAY': 'gateway.sandbox.push.apple.com',
+		'GCM_TIMETOLIVE': 3000,	// Duration in seconds to hold in GCM and retry before timing out.
+		'MONGO_CONNECTION_DIR' : '127.0.0.1:27017/mongojsdb',
+		'MONGO_DB_NAME' : 'logs'
+	};
+	return {
+		get: function(name) { return private[name]; }
+    };
+})();
+
+
+module.exports.get = SETTINGS.get;
+
+
