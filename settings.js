873d41711848b394af67b12c1cffbff1b13f551f
MikelDelTio <Mikeldeltio@outlook.com>
2014-02-02 18:10:02 +0100

    Async problem resolved
    Works OK with Android.

--- settings.js
+++ settings.js
@@ -1,7 +1,8 @@
 var SETTINGS = (function() {
 	var private = {
-		'GCM_ID': 'INSERT YOUT GCN_ID',	
+		'GCM_ID': 'AIzaSyCiWYVCwBJVfjg3Y9-CQm4VMe4eO1zsGgM',	//MUST be changed with you GCN_ID
 		'GCM_MSGCNT': '1',
+		'GCM_RETRIES': 4,
 		'APN_GATEWAY': 'gateway.sandbox.push.apple.com',
 		'GCM_TIMETOLIVE': 3000,	// Duration in seconds to hold in GCM and retry before timing out.
 		'MONGO_CONNECTION_DIR' : '127.0.0.1:27017/mongojsdb',
