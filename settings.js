381f7476d3f8a505d8d2031cd5ac20f9e4a74e6e
Mikel <Mikeldeltio@outlook.com>
2014-02-05 17:33:06 +0100

    Extended

--- settings.js
+++ settings.js
@@ -3,17 +3,15 @@ var SETTINGS = (function() {
 		'GCM_ID': 'AIzaSyCiWYVCwBJVfjg3Y9-CQm4VMe4eO1zsGgM',	//MUST be changed with you GCN_ID
 		'GCM_MSGCNT': '1',
 		'GCM_RETRIES': 4,
-		'APN_GATEWAY': 'gateway.sandbox.push.apple.com',
 		'GCM_TIMETOLIVE': 3000,	// Duration in seconds to hold in GCM and retry before timing out.
-		'MONGO_CONNECTION_DIR' : '127.0.0.1:27017/mongojsdb',
-		'MONGO_DB_NAME' : 'logs'
+		'GCM_DELAYWHILEIDLE' : false,
+		'APN_GATEWAY': 'gateway.sandbox.push.apple.com',
+		'APN_EXPIRY' : 3600, // Duration in seconds before expiry
+		'APN_SOUND': 'ping.aiff',
 	};
 	return {
 		get: function(name) { return private[name]; }
     };
 })();
 
-
 module.exports.get = SETTINGS.get;
-
-
