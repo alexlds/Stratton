9e511fe02847aaf94aa781c76f784ab0a15f14f1
Mikel <Mikeldeltio@outlook.com>
2014-02-09 19:48:36 +0100

    Blackberry unsupported

--- settings.js
+++ settings.js
@@ -7,12 +7,7 @@ var SETTINGS = (function() {
 		'GCM_DELAYWHILEIDLE' : false,
 		'APN_GATEWAY': 'gateway.sandbox.push.apple.com',
 		'APN_EXPIRY' : 3600, // Duration in seconds before expiry
-		'APN_SOUND': 'ping.aiff',
-		'BB10_APPID' : 'PUT-YOUR-BB10-APP-ID',
-		'BB10_PASSWORD' : 'PUT-YOUR-BB10-APP-BB10_PASSWORD',
-		'BB10_CPID' : 'PUT-YOUR-BB10-APP-CPID',
-		'BB10_APPNAME' : 'PUT-YOUR-BB10-APP-NAME',
-		'BB10_DELIVERYMETHOD' : 'unconfirmed'
+		'APN_SOUND': 'ping.aiff'
 	};
 	return {
 		get: function(name) { return private[name]; }
