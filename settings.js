4d7f93bc4a9587c2bcb3a2b9bb3e79a873d0a9a0
Mikel <Mikeldeltio@outlook.com>
2014-02-13 08:08:58 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,10 +3,10 @@ var SETTINGS = (function() {
 		'GCM_ID': 'PUT-YOUR-GCM-SERVER-API-KEY',	
 		'GCM_MSGCNT': '1',
 		'GCM_RETRIES': 4,
-		'GCM_TIMETOLIVE': 3000,	// Duration in seconds to hold in GCM and retry before timing out.
+		'GCM_TIMETOLIVE': 3000,	
 		'GCM_DELAYWHILEIDLE' : false,
 		'APN_GATEWAY': 'gateway.sandbox.push.apple.com',
-		'APN_EXPIRY' : 3600, // Duration in seconds before expiry
+		'APN_EXPIRY' : 3600, 
 		'APN_SOUND': 'ping.aiff'
 	};
 	return {
